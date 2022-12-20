b2a411d93685bad221b14face91ae2ff08591c82
Lee Sang Hoon <nicet.shlee@gmail.com>
2022-12-20 19:26:41 +0900

    예외처리 추가

---
+++ src/main/java/com/example/team3_miniproject/exception/RequestException.java
@@ -0,0 +1,16 @@
+package com.example.team3_miniproject.exception;
+
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class RequestException extends RuntimeException {
+
+    private final HttpStatus httpStatus;
+
+    public RequestException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.httpStatus = errorCode.getHttpStatus();
+    }
+}
